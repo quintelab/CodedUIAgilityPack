b0fb883e2e8eda11efc731a4c2fd05a49da654e6
Bruno Quintella <bquinte@Allianzire.ie>
2017-09-13 08:59:04 +0100

    Add a MVC project with 5 views: Button, CheckBox, DropDownLilst, RadioButton and Textbox. These views will be tested by CodedUiAgilityPack

---
+++ CodedUIAgilityPack.Web/Controllers/DropDownListController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CodedUIAgilityPack.Web.Controllers
+{
+    public class DropDownListController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CodedUIAgilityPack.Web/Controllers/TextBoxController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CodedUIAgilityPack.Web.Controllers
+{
+    public class TextBoxController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
